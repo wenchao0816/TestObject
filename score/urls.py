354f9194247f2bf5816a7951580efab93b6c749f
apple <519738668@qq.com>
2020-07-08 16:55:27 +0800

    'TestObject'

--- score/urls.py
+++ score/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ScoreView, IndexView
 
 urlpatterns = [
-    path('index', IndexView.as_view(), name='sorted'),
+    path('index', IndexView.as_view(), name='index'),
     path('', ScoreView.as_view(), name='sorted')
 ]
